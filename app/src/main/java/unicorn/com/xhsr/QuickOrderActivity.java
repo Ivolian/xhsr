cb21b7875c7d5a78791904ce6029860d028ff001
renjiajian <renjiajian@withub.net.cn>
2016-01-15 15:02:39 +0800

    添加语音功能，代码待重构。

--- app/src/main/java/unicorn/com/xhsr/QuickOrderActivity.java
+++ app/src/main/java/unicorn/com/xhsr/QuickOrderActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,14 +18,28 @@ import com.flipboard.bottomsheet.BottomSheetLayout;
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 import com.github.florent37.viewanimator.ViewAnimator;
 import com.github.ppamorim.dragger.DraggerActivity;
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.RecognizerListener;
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.simple.eventbus.EventBus;
 import org.simple.eventbus.Subscriber;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import butterknife.Bind;
 import butterknife.OnClick;
 import unicorn.com.xhsr.groupselect.GroupSelectHelper;
+import unicorn.com.xhsr.speech.JsonParser;
+import unicorn.com.xhsr.utils.ToastUtils;
 
 
 public class QuickOrderActivity extends DraggerActivity {
@@ -45,12 +60,51 @@ public class QuickOrderActivity extends DraggerActivity {
     public void onDestroy() {
         EventBus.getDefault().unregister(this);
         super.onDestroy();
+        mIat.cancel();
+        mIat.destroy();
     }
 
     private void initViews() {
         initEquipment();
         initBottomSheet();
+
+        mIat = SpeechRecognizer.createRecognizer(this, mInitListener);
+
     }
+
+    public void setParam() {
+        // 清空参数
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+
+        // 设置听写引擎
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+
+
+
+            // 设置语言
+            mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+            // 设置语言区域
+            mIat.setParameter(SpeechConstant.ACCENT,"mandarin");
+
+
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS,  "4000");
+
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS,  "1000");
+
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT,  "1");
+
+
+
+        // 设置听写结果是否结果动态修正，为“1”则在听写过程中动态递增地返回结果，否则只在听写结束之后返回最终结果
+        // 注：该参数暂时只对在线听写有效
+        mIat.setParameter(SpeechConstant.ASR_DWA, "0");
+    }
+
 
 
     // =============================== 选择需要维修的设备 ===============================
@@ -154,11 +208,99 @@ public class QuickOrderActivity extends DraggerActivity {
 
     // =============================== 补充说明 ===============================
 
+    @Bind(R.id.etDescription)
+    EditText etDescription;
+
+    private SpeechRecognizer mIat;
+
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            if (code != ErrorCode.SUCCESS) {
+                ToastUtils.show("初始化失败，错误码：" + code);
+            }
+        }
+    };
+
+
     @Bind(R.id.descriptionArrow)
     ImageView ivDescriptionArrow;
 
     @Bind(R.id.elDescription)
     ExpandableRelativeLayout elDescription;
+    int ret = 0;
+
+    private RecognizerListener mRecognizerListener = new RecognizerListener() {
+
+        @Override
+        public void onBeginOfSpeech() {
+            // 此回调表示：sdk内部录音机已经准备好了，用户可以开始语音输入
+            ToastUtils.show("开始说话");
+        }
+
+        @Override
+        public void onError(SpeechError error) {
+            // Tips：
+            // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
+            // 如果使用本地功能（语记）需要提示用户开启语记的录音权限。
+            ToastUtils.show(error.getPlainDescription(true));
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 此回调表示：检测到了语音的尾端点，已经进入识别过程，不再接受语音输入
+            ToastUtils.show("结束说话");
+        }
+
+        @Override
+        public void onResult(RecognizerResult results, boolean isLast) {
+
+
+           printResult(results);
+
+
+        }
+
+        @Override
+        public void onVolumeChanged(int volume, byte[] data) {
+            ToastUtils.show("当前正在说话，音量大小：" + volume);
+        }
+
+        @Override
+        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
+            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
+            // 若使用本地能力，会话id为null
+            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
+            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
+            //		Log.d(TAG, "session id =" + sid);
+            //	}
+        }
+    };
+
+    private HashMap<String, String> mIatResults = new LinkedHashMap<String, String>();
+
+    private void printResult(RecognizerResult results) {
+        String text = JsonParser.parseIatResult(results.getResultString());
+
+        String sn = null;
+        // 读取json结果中的sn字段
+        try {
+            JSONObject resultJson = new JSONObject(results.getResultString());
+            sn = resultJson.optString("sn");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mIatResults.put(sn, text);
+
+        StringBuffer resultBuffer = new StringBuffer();
+        for (String key : mIatResults.keySet()) {
+            resultBuffer.append(mIatResults.get(key));
+        }
+
+        etDescription.setText(resultBuffer.toString());
+    }
 
     @OnClick(R.id.description)
     public void descriptionOnClick() {
@@ -168,6 +310,14 @@ public class QuickOrderActivity extends DraggerActivity {
                 .duration(300)
                 .start();
         elDescription.toggle();
+
+        setParam();
+        ret = mIat.startListening(mRecognizerListener);
+        if (ret != ErrorCode.SUCCESS) {
+            ToastUtils.show("听写失败,错误码：" + ret);
+        } else {
+            ToastUtils.show("请开始说话");
+        }
     }
 
 
--- app/src/main/java/unicorn/com/xhsr/SimpleApplication.java
+++ app/src/main/java/unicorn/com/xhsr/SimpleApplication.java
@@ -2,6 +2,8 @@ package unicorn.com.xhsr;
 
 import android.app.Application;
 
+import com.iflytek.cloud.SpeechUtility;
+
 
 public class SimpleApplication extends Application {
 
@@ -13,6 +15,8 @@ public class SimpleApplication extends Application {
 
     @Override
     public void onCreate() {
+
+        SpeechUtility.createUtility(this, "appid=" + "5697584f");
 
         super.onCreate();
         instance = this;
